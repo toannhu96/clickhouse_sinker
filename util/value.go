8c0cc11ceb765ba815603b45187c5a53cfbbe641
toannhu96 <toan.nhu@tiki.vn>
2020-06-02 17:50:39 +0700

    update Date type with string

--- util/value.go
+++ util/value.go
@@ -52,15 +52,15 @@ func GetValueByType(metric model.Metric, cwt *model.ColumnWithType) interface{} 
 
 func switchType(typ string) string {
 	switch typ {
-	case "Date", "DateTime", "UInt8", "UInt16", "UInt32", "UInt64", "Int8",
-		"Int16", "Int32", "Int64", "Nullable(Date)", "Nullable(DateTime)",
+	case "DateTime", "UInt8", "UInt16", "UInt32", "UInt64", "Int8",
+		"Int16", "Int32", "Int64", "Nullable(DateTime)",
 		"Nullable(UInt8)", "Nullable(UInt16)", "Nullable(UInt32)", "Nullable(UInt64)",
 		"Nullable(Int8)", "Nullable(Int16)", "Nullable(Int32)", "Nullable(Int64)":
 		return "int"
 	case "Array(Date)", "Array(DateTime)", "Array(UInt8)", "Array(UInt16)", "Array(UInt32)",
 		"Array(UInt64)", "Array(Int8)", "Array(Int16)", "Array(Int32)", "Array(Int64)":
 		return "intArray"
-	case "String", "FixString", "Nullable(String)":
+	case "Date", "Nullable(Date)", "String", "FixString", "Nullable(String)":
 		return "string"
 	case "Array(String)", "Array(FixString)":
 		return "stringArray"
